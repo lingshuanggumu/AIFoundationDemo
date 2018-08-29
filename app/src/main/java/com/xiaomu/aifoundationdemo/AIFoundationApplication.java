2ddec4b7c6255e4dd4f5620df26e7d907b30d6f9
lingshuanggumu <lingshuanggumu@163.com>
2018-08-30 01:14:32 +0800

    Description:Setup project structure

---
+++ app/src/main/java/com/xiaomu/aifoundationdemo/AIFoundationApplication.java
@@ -0,0 +1,16 @@
+package com.xiaomu.aifoundationdemo;
+
+import android.app.Application;
+
+public class AIFoundationApplication extends Application {
+    private static final String TAG = "AIFoundationApplication";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
